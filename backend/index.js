de3fd806ebd27f9174cfb77e4971f110c2fbc5a5
saculgg <lucas.abreuamorim@gmail.com>
2025-08-22 23:52:49 -0300

    indexjs

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Frontend URL
+  origin: 'https://top-acai-site.vercel.app/', // Frontend URL
   credentials: true
 }));
 app.use(express.json());
